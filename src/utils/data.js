39d4a75b382ac347fb2005fedc8b16e8a474a22d
Jorge Machuca <jmachuca06@gmail.com>
2023-05-15 15:02:00 -0400

    dynamic image url

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,30 +1,93 @@
 export const pageLinks = [
-  { href: "#home", text: "home" },
-  { href: "#about", text: "about" },
-  { href: "#services", text: "services" },
-  { href: "#tours", text: "tours" },
+	{ href: "#home", text: "home" },
+	{ href: "#about", text: "about" },
+	{ href: "#services", text: "services" },
+	{ href: "#tours", text: "tours" },
 ];
 
 export const socialLinks = [
-  { href: "https://www.twitter.com", icon: "fab fa-facebook" },
-  { href: "https://www.twitter.com", icon: "fab fa-twitter" },
-  { href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+	{ href: "https://www.twitter.com", icon: "fab fa-facebook" },
+	{ href: "https://www.twitter.com", icon: "fab fa-twitter" },
+	{ href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
 
 export const services = [
-  {
-    icon: "fas fa-wallet fa-fw",
-    title: "saving money",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
-  },
-  {
-    icon: "fas fa-tree fa-fw",
-    title: "endless hiking",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
-  },
-  {
-    icon: "fas fa-socks fa-fw",
-    title: "amazing comfort",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
-  },
+	{
+		icon: "fas fa-wallet fa-fw",
+		title: "saving money",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+	},
+	{
+		icon: "fas fa-tree fa-fw",
+		title: "endless hiking",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+	},
+	{
+		icon: "fas fa-socks fa-fw",
+		title: "amazing comfort",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+	},
+];
+
+export const tours = [
+	{
+		image: "tour-1.jpeg",
+		date: "august 26th, 2020",
+		title: "Tibet Adventure",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "china",
+		duration: "6",
+		cost: "2100",
+	},
+	{
+		image: "tour-2.jpeg",
+		date: "october 1th, 2020",
+		title: "best of java",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		duration: "11",
+		cost: "1400",
+	},
+	{
+		image: "tour-3.jpeg",
+		date: "october 1th, 2020",
+		title: "best of java",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		duration: "11",
+		cost: "1400",
+	},
+	{
+		image: "tour-4.jpeg",
+		date: "october 1th, 2020",
+		title: "best of java",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		duration: "11",
+		cost: "1400",
+	},
+	{
+		image: "tour-5.jpeg",
+		date: "october 1th, 2020",
+		title: "best of java",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		duration: "11",
+		cost: "1400",
+	},
+	{
+		image: "tour-6.jpeg",
+		date: "october 1th, 2020",
+		title: "best of java",
+		info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		duration: "11",
+		cost: "1400",
+	},
 ];
